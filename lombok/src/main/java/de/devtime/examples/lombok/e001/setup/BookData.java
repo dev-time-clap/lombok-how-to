63222c2c65f7cb57d2d122e8c0a3f35cb972f2a2
Morrigan <dev.time.tpw@gmail.com>
2026-01-01 16:30:53 +0100

    Examples updated

---
+++ lombok/src/main/java/de/devtime/examples/lombok/e001/setup/BookData.java
@@ -0,0 +1,22 @@
+package de.devtime.examples.lombok.e001.setup;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder(setterPrefix = "with")
+public class BookData {
+
+  private String isbn;
+  private String title;
+  private String author;
+
+}
